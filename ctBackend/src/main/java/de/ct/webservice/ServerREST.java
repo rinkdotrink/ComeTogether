812b3aeaa51f1927550d1a565ddc3921d04011cf
Ingo Koerner <ingo.koerner@googlemail.com>
2012-04-14 17:26:59 +0200

    Message

--- ctBackend/src/main/java/de/ct/webservice/ServerREST.java
+++ ctBackend/src/main/java/de/ct/webservice/ServerREST.java
@@ -1,6 +1,31 @@
 package de.ct.webservice;
 
+
 public interface ServerREST {
 
 	public String halloWelt();
+
+	// public void User(String name, String gender, String sex, String password,
+	// String email, Picture picture);
+	//
+	// public User User(String name, String password);
+
+	// DELETE User(name : String, password : String) : User
+	//
+	// GET Events(date : Date, location : String, gps : int[], occasion :
+	// String, perimeter : int) : List<Event>
+	//
+	// GET Event(eventId : long) : Event
+	//
+	// POST Event(date : Date, eventName : String, location : String, occasion :
+	// String, description : String, numberMaleConfirmed : int,
+	// numberFemaleConfirmed : int) : void
+	//
+	// POST Event(eventId : long, date : Date, eventName : String, location :
+	// String, occasion : String, description : String, numberMaleConfirmed :
+	// int, numberFemaleConfirmed : int) : void
+	//
+	// DELETE Event(eventId : long) : void
+	//
+
 }
--- ctBackend/src/main/java/de/ct/webservice/ServerRESTImpl.java
+++ ctBackend/src/main/java/de/ct/webservice/ServerRESTImpl.java
@@ -1,15 +1,67 @@
 package de.ct.webservice;
 
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import com.google.inject.Inject;
+
+import de.ct.datamodel.Message;
+import de.ct.service.MessageService;
+
 @Path("/ct")
-public class ServerRESTImpl implements ServerREST {
+public class ServerRESTImpl  implements ServerREST {
+
+	MessageService messageService;
+
+	@Inject
+	public ServerRESTImpl(MessageService aMessageService) {
+		this.messageService = aMessageService;
+	}
+
+	@GET
+	@Produces("text/xml")
+	public List<Message> Messages() {
+		List<Message> messages = messageService.getMessages();
+		return messages;
+	}
+
+	@GET
+	@Produces("text/xml")
+	@Consumes("text/xml")
+	public Message Message(long messageId) {
+		Message message = messageService.findMessageById(messageId);
+		return message;
+	}
+
+	@POST
+	@Produces("text/xml")
+	@Consumes("text/xml")
+	public void Message(String aTextMessage, long aEventId, long aSenderId,
+			long aRecipientId) {
+
+		Message message = messageService.createNewMessage();
+		message.setTextmessage(aTextMessage);
+		message.setSenderId(aSenderId);
+		message.setRecipientId(aRecipientId);
+		message.setEventId(aEventId);
+		messageService.saveMessage(message);
+	}
+
+	@DELETE
+	public void DELETEMessage(long messageId) {
+		messageService.deleteMessage(messageId);
+	}
 
 	@GET
 	@Produces("text/xml")
 	public String halloWelt() {
 		return "Hallo Welt!!";
 	}
+
 }
