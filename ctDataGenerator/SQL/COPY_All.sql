d0e15b999d7184dd37b98d676685ecd66a2d0453
Ingo Koerner <ingo.koerner@googlemail.com>
2012-06-16 00:18:36 +0200

    all

---
+++ ctDataGenerator/SQL/COPY_All.sql
@@ -0,0 +1,4 @@
+﻿COPY public.Event (eventid, creatorid, date, eventname, occasion, location, lonRad, latRad, description, numbermaleconfirmed, numberfemaleconfirmed) From 'C:\Event.txt' DELIMITER ';';
+COPY public.Message (messageid, eventid, senderid, recipientid, textmessage, date) From 'C:\Message.txt' DELIMITER ';';
+COPY public.Participation (participationid, userid, eventid) From 'C:\Participation.txt' DELIMITER ';';
+COPY public.User (userId, name, email, gender, birthday, password, image) From 'C:\User.txt' DELIMITER ';';
