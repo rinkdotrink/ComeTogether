558b81d3aa0c24617e2d9de4252a6c3b9b5e440a
Ingo Koerner <ingo.koerner@googlemail.com>
2012-06-17 12:01:57 +0200

    rename

--- ctAndroidClient/gen/net/neilgoodman/android/restservicetutorial/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package net.neilgoodman.android.restservicetutorial;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- ctAndroidClient/src/de/ct/client/RESTServiceActivity.java
+++ ctAndroidClient/src/de/ct/client/RESTServiceActivity.java
@@ -1,6 +1,5 @@
 package de.ct.client;
 
-import net.neilgoodman.android.restservicetutorial.R;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
